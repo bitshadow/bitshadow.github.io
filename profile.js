ae189bfad30eddfa80d599901b2f6c191525a0b7
Jignesh Kakadiya <jigneshhk1992@gmail.com>
2013-12-14 14:48:11 +0530

    animate

--- profile.js
+++ profile.js
@@ -1,11 +1,42 @@
 window.onload = function(){
   var listItems = $( 'li' );
   var a = [1, 2, 3, 5, 8, 7];
-  var count = 0;
+  var b = [4, 6, 9];
+  var colorClasses = [
+    'blue',
+    'green',
+    'orange',
+    'purple',
+    'red',
+  ];
 
-  setInterval(function(){
-    var $listItem = listItems.eq(a[count]);
-    $listItem.hasClass('push') ? $listItem.removeClass('push') : $listItem.addClass('push');
+  var count = 0;
+  ApplyExtras();
+  var tid = setTimeout(shine, 1000);
+
+  function ApplyExtras() {
+    for (var i = 0; i < b.length; i++) {
+      applyEffect(listItems.eq(b[i]));
+    };
+  }
+
+  function applyEffect(element){
+    element.hasClass('push')?
+    element.removeClass('push'):
+    element.addClass('push');
+  }
+
+  function shine() {
+    applyEffect(listItems.eq(a[count]))
+
     count = ++count % a.length;
-  },2000);
+
+    if (count == 0) {
+      applyEffect(listItems.eq(0));
+      ApplyExtras();
+    }
+
+    clearTimeout(tid);
+    tid = setTimeout(shine, 2000);
+  }
 };
